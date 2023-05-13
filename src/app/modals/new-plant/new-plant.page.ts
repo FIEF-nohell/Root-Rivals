852eb131a23f866555949ea00cb4845c4c1317d8
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2023-05-13 02:38:01 +0200

    new plant design

--- src/app/modals/new-plant/new-plant.page.ts
+++ src/app/modals/new-plant/new-plant.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-new-plant',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPlantPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {
+  activePlant: string = ""
+
+  async ngOnInit() {
+    
+  }
+
+  async choosePlant(plant: string){
+    const alert = await this.alertController.create({
+      header: plant, 
+      message: 'Are you sure you want to choose this as your plant?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            
+          },
+        },
+        {
+          text: 'Yes',
+          role: 'confirm',
+          handler: () => {
+            this.activePlant = plant
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
 }
