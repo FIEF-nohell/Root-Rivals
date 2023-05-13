55f9c0c08491f380fd5362ffaae4e59921574eec
FIEF-nohell <noel.hmnn@gmail.com>
2023-05-13 14:52:54 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBqTIVzggpIbBqlyY-w-mdtsKX7VcqAiTk",
+    authDomain: "root-rivals.firebaseapp.com",
+    projectId: "root-rivals",
+    storageBucket: "root-rivals.appspot.com",
+    messagingSenderId: "821994331348",
+    appId: "1:821994331348:web:ccea4450f5c90c507eaf41",
+    measurementId: "G-8EQ3YP8ZRK"
+  }
 };
